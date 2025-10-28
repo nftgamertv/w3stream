97d2a0dad81034d1891940b2693cbfd3e722601b
trentdavis78 <trentdavisinc@gmail.com>
2025-10-28 14:18:51 -0500

    backstage fix

--- src/app/[...puckPath]/client.tsx
+++ src/app/[...puckPath]/client.tsx
@@ -3,6 +3,7 @@
 import { Render } from "@measured/puck";
 import config from "@/config/puck.config";
 import LivekitRoomWrapper from "@/providers/LivekitRoomWrapper";
+import { usePathname } from "next/navigation";
 
 interface ClientProps {
   data: any;
@@ -11,11 +12,17 @@ interface ClientProps {
 export const Client: React.FC<ClientProps> = ({ data }) => {
   console.log('Client component received data:', data);
 
+  // Extract roomId from URL path
+  const pathname = usePathname();
+  const roomId = pathname?.replace(/^\//, '') || 'default-room';
+
+  console.log('Client component using roomId from path:', roomId);
+
   if (!data) {
     console.error('Client component: No data provided');
     return <div className="p-8">No data to render</div>;
   }
 
-  return <LivekitRoomWrapper roomId={data.roomId}><Render config={config} data={data} />;</LivekitRoomWrapper>
+  return <LivekitRoomWrapper roomId={roomId}><Render config={config} data={data} />;</LivekitRoomWrapper>
 };
  
